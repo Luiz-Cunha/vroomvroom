88cdcd9937f2fd77cbb61d03bb854f73f7f81d40
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-06-04 14:44:43 -0400

    Motorcycle new and create actions

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -6,9 +6,17 @@ class MotorcyclesController < ApplicationController
   end
 
   def new
+    @motorcycle = Motorcycle.new
   end
 
   def create
+    @motorcycle = Motorcycle.new(motorcycle_params)
+    @motorcycle.user = current_user
+    if @motorcycle.save
+      redirect_to motorcycles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,5 +26,10 @@ class MotorcyclesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def motorcycle_params
+    params.require(:motorcycle).permit(:make, :model, :typeM, :year, :description, :photo)
   end
 end
