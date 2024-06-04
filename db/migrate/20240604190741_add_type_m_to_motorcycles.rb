6b852cf7160ca47bda1ca40029e67afab91b7878
Munick Souza <munick.freitas@hotmail.com>
2024-06-04 14:21:27 -0600

    Fixing the migration

--- db/migrate/20240604190741_add_type_m_to_motorcycles.rb
+++
@@ -1,6 +0,0 @@
-class AddTypeMToMotorcycles < ActiveRecord::Migration[7.1]
-  def change
-    add_column :motorcycles, :typeM, :string
-    remove_column :motorcycles, :type, :string if column_exists?(:motorcycles, :type)
-  end
-end
