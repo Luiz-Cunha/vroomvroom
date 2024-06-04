492a129251e4cea8faed0e572085d28a80ce66d6
Munick Souza <munick.freitas@hotmail.com>
2024-06-04 13:51:31 -0600

    Added edit and update

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -1,4 +1,5 @@
 class MotorcyclesController < ApplicationController
+
   def index
   end
 
@@ -12,11 +13,29 @@ class MotorcyclesController < ApplicationController
   end
 
   def edit
+    @motorcycle = Motorcycle.find(params[:id])
+
   end
 
   def update
+    @motorcycle = Motorcycle.find(params[:id])
+    if @motorcycle.update(motorcycle_params)
+      redirect_to @motorcycle, notice: "Motorcycle was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def motorcycle_params
+    params.require(:motorcycle).permit(:image_url, :description, :make, :model, :year, :user_id)
+  end
+
+  def set_motorcycle
+    @motorcycle = Motorcycle.find(params[:id])
   end
 end
