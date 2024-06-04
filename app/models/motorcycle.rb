9c8134d2b21a098abe26b64121cb15f2bad93dce
Eemon Roy <poyshaproductions@gmail.com>
2024-06-04 21:49:45 +0530

    added functionality to show the motorcycles individually

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,4 +1,8 @@
 class Motorcycle < ApplicationRecord
   belongs_to :user
   has_many :rent_motorcycles
+
+  def show
+    @motorcycle = Motorcycle.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "motorcycles/:id", to: "motorcycles#show"
 end
